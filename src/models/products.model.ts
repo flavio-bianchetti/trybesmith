0277cd7b50d64f9c855e1722dc0f57ed9b8c6469
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-29 20:01:44 -0300

    feat: create model method included

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,4 +1,4 @@
-import { Pool, RowDataPacket } from 'mysql2/promise';
+import { Pool, RowDataPacket, ResultSetHeader } from 'mysql2/promise';
 import { IProduct } from '../interfaces/IProduct';
 
 export default class ProductsModel {
@@ -12,5 +12,16 @@ export default class ProductsModel {
     const [result] = await this.connection
       .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.Products;');
     return result as IProduct[];
+  };
+
+  public create = async (product: IProduct): Promise<IProduct> => {
+    const { name, amount } = product;
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?);',
+      [name, amount],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...product };
   };
 }
