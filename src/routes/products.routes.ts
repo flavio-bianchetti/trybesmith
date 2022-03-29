6350db177551692de07154e2d06bf7f3f7ad1e4d
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-29 20:11:52 -0300

    feat: products validation middleware included in routes

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller';
+import productsMiddleware from '../middlewares/products.middleware';
 
 const router = Router();
 
 const productsController = new ProductsController();
 
 router.get('/products', productsController.getAll);
+
+router.post('/products', productsMiddleware, productsController.create);
 
 export default router;
