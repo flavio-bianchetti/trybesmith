9030fe2eef772de4b1bc67a2a8d30798c652454c
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-29 22:59:42 -0300

    feat: create user service method included

---
+++ src/services/users.service.ts
@@ -0,0 +1,26 @@
+import connection from '../models/connection';
+import UsersModel from '../models/users.model';
+import { IUser } from '../interfaces/IUser';
+import tokenGenerator from '../util/auth/tokenGenerator';
+
+export default class UsersService {
+  private usersModel: UsersModel;
+
+  constructor() {
+    this.usersModel = new UsersModel(connection);
+  }
+
+  public create = async (user: IUser): Promise<{ token: string } | undefined> => {
+    const { username } = user;
+
+    const result = await this.usersModel.findByName(username);
+
+    if (result) return undefined;
+
+    const newUser = await this.usersModel.create(user);
+    
+    if (!newUser) return undefined;
+
+    return tokenGenerator(username);
+  };
+}
