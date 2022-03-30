e21106f57779be116ee4a65f3866933234794374
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-29 22:58:10 -0300

    feat: create & findByName model methods included

---
+++ src/models/users.model.ts
@@ -0,0 +1,33 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { IUser } from '../interfaces/IUser';
+
+export default class UserModel {
+  private connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public findByName = async (userName: string): Promise<IUser> => {
+    const result = await this.connection.execute(
+      'SELECT * FROM Trybesmith.Users WHERE username = ?;',
+      [userName],
+    );
+    const [rows] = result;
+    const [user] = rows as IUser[];
+    return user;
+  };
+
+  public create = async (user: IUser): Promise<IUser> => {
+    const { username, classe, level, password } = user;
+
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?);',
+      [username, classe, level, password],
+    );
+
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...user };
+  };
+}
