7f57beb65616b06b8e20bfbb5f8ba1c89b5c485a
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-31 07:53:44 -0300

    feat: POST /orders route included

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import OrdersController from '../controllers/orders.controller';
+import validTokenMiddleware from '../middlewares/validToken.middleware';
+import productsValidator from '../middlewares/orders.middleware';
 
 const router = Router();
 
 const ordersController = new OrdersController();
 
 router.get('/orders', ordersController.getAll);
+router.post('/orders', validTokenMiddleware, productsValidator, ordersController.create);
 
 export default router;
