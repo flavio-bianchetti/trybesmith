398b4a93263763ad04f99dc55c9d50c13660b680
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-01 15:41:29 -0300

    refactor: order service - type handling changed

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -2,6 +2,7 @@ import connection from '../models/connection';
 import OrdersModel from '../models/orders.model';
 import ProductsModel from '../models/products.model';
 import { IOrder } from '../interfaces/IOrder';
+import { IOrderResultCreate } from '../interfaces/IOrderResultCreate';
 
 export default class OrdersService {
   private ordersModel: OrdersModel;
@@ -13,10 +14,11 @@ export default class OrdersService {
     this.productsModel = new ProductsModel(connection);
   }
 
-  public getAll = async (): Promise<IOrder[]> =>
+  public getAll: () => Promise<IOrder[] | undefined> = async (): Promise<IOrder[] | undefined> =>
     this.ordersModel.getAll();
   
-  public create = async (products: number[], userId: number) => {
+  public create: (products: number[], userId: number) => Promise<IOrderResultCreate | undefined>
+  = async (products, userId): Promise<IOrderResultCreate | undefined> => {
     const existProducts = Promise
       .all(products.map(async (product) => this.productsModel.getById(product)));
 
@@ -27,10 +29,10 @@ export default class OrdersService {
     const order = await this.ordersModel.create(userId);
 
     const productsUpdated = Promise.all(products.map(async (product) => {
-      const getProduct = productsList.find((p) => p.id === product);
+      const getProduct = productsList.find((p) => p?.id === product);
       if (getProduct) {
         const { name, amount } = getProduct;
-        return this.productsModel.update({ id: product, name, amount, orderId: order.id });
+        return this.productsModel.update({ id: product, name, amount, orderId: order?.id });
       }
     }));
 
