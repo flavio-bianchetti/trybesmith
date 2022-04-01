cc4f76fa81117414e7c5fea27ba178851a092969
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-01 15:44:27 -0300

    refactor: order controller - type handling changed

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 import { RequestCustom } from '../interfaces/RequestCustom';
 import OrdersService from '../services/orders.service';
 import { IOrder } from '../interfaces/IOrder';
+import { IProductsCreate } from '../interfaces/IProductsCreate';
 
 export default class ProductsController {
   private ordersService: OrdersService;
@@ -11,21 +12,27 @@ export default class ProductsController {
     this.ordersService = new OrdersService();
   }
 
-  public getAll = async (_req: Request, res: Response): Promise<void> => {
-    const orders: IOrder[] = await this.ordersService.getAll();
-    res.status(StatusCodes.OK).json(orders);
+  public getAll: (req: Request, res: Response) => Promise<void>
+  = async (_req, res): Promise<void> => {
+    const orders: IOrder[] | undefined = await this.ordersService.getAll();
+    if (!orders) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+    } else {
+      res.status(StatusCodes.OK).json(orders);
+    }
   };
 
   // solução encontrada em:
   // https://stackoverflow.com/questions/37377731/extend-express-request-object-using-typescript
-  public create = async (req: RequestCustom, res: Response): Promise<void> => {
-    const { products } = req.body;
-    const id = req.userId || 0;
+  public create: (req: RequestCustom, res: Response) => Promise<void>
+  = async (req, res): Promise<void> => {
+    const { products }: IProductsCreate = req.body;
+    const id: number = req.userId || 0;
     const order = await this.ordersService.create(products, id);
-    if (order) {
-      res.status(StatusCodes.CREATED).json(order);
-    } else {
+    if (!order) {
       res.status(StatusCodes.NOT_FOUND).json({ message: 'Order not found' });
+    } else {
+      res.status(StatusCodes.CREATED).json(order);
     }
   };
 }
