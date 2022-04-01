42420c4fb5ec8d0182d0549b091a2fcc5b625727
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-01 15:34:25 -0300

    refactor: users model - type handling changed

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -8,36 +8,54 @@ export default class UserModel {
     this.connection = connection;
   }
 
-  public findByName: (userName: string) => Promise<IUser> = async (userName): Promise<IUser> => {
-    const result = await this.connection.execute<RowDataPacket[]>(
-      'SELECT * FROM Trybesmith.Users WHERE username = ?;',
-      [userName],
-    );
-    const [rows] = result;
-    const [user] = rows as IUser[];
-    return user;
+  public findByName: (userName: string) => Promise<IUser | undefined>
+  = async (userName): Promise<IUser | undefined> => {
+    try {
+      const result = await this.connection.execute<RowDataPacket[]>(
+        'SELECT * FROM Trybesmith.Users WHERE username = ?;',
+        [userName],
+      );
+      const [rows] = result;
+      const [user] = rows as IUser[];
+      return user;
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   };
 
-  public findById: (userId: number) => Promise<IUser> = async (userId): Promise<IUser> => {
-    const result = await this.connection.execute<RowDataPacket[]>(
-      'SELECT * FROM Trybesmith.Users WHERE id = ?;',
-      [userId],
-    );
-    const [rows] = result;
-    const [user] = rows as IUser[];
-    return user;
+  public findById: (userId: number) => Promise<IUser | undefined>
+  = async (userId): Promise<IUser | undefined> => {
+    try {
+      const result = await this.connection.execute<RowDataPacket[]>(
+        'SELECT * FROM Trybesmith.Users WHERE id = ?;',
+        [userId],
+      );
+      const [rows] = result;
+      const [user] = rows as IUser[];
+      return user;
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   };
 
-  public create: (user: IUser) => Promise<IUser> = async (user): Promise<IUser> => {
-    const { username, classe, level, password } = user;
+  public create: (user: IUser) => Promise<IUser | undefined>
+  = async (user): Promise<IUser | undefined> => {
+    try {
+      const { username, classe, level, password } = user;
 
-    const result = await this.connection.execute<ResultSetHeader>(
-      'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?);',
-      [username, classe, level, password],
-    );
+      const result = await this.connection.execute<ResultSetHeader>(
+        'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?);',
+        [username, classe, level, password],
+      );
 
-    const [dataInserted] = result;
-    const { insertId } = dataInserted;
-    return { id: insertId, ...user };
+      const [dataInserted] = result;
+      const { insertId } = dataInserted;
+      return { id: insertId, ...user };
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   };
 }
