6eb492453a858b1a1a02916d518fdbe4ea5f99f5
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-01 15:36:42 -0300

    refactor: orders model - type handling changed

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -1,7 +1,6 @@
 import { Pool, RowDataPacket, ResultSetHeader } from 'mysql2/promise';
 import { IOrder } from '../interfaces/IOrder';
 import { IOrderResult } from '../interfaces/IOrderResult';
-// import { IProduct } from '../interfaces/IProduct';
 
 export default class OrderModel {
   private connection: Pool;
@@ -10,29 +9,41 @@ export default class OrderModel {
     this.connection = connection;
   }
 
-  public getAll: () => Promise<IOrderResult[]> = async (): Promise<IOrderResult[]> => {
-    const [rows] = await this.connection.execute<RowDataPacket[]>(
-      `SELECT o.id, o.userId, JSON_ARRAYAGG(p.id) AS products
-      FROM Trybesmith.Orders o
-      INNER JOIN Trybesmith.Products p ON o.id = p.orderId
-      GROUP BY o.userId, o.id;`,
-    );
-
-    const orders = rows as IOrderResult[];
-
-    return orders;
+  public getAll: () => Promise<IOrderResult[] | undefined>
+  = async (): Promise<IOrderResult[] | undefined> => {
+    try {
+      const [rows] = await this.connection.execute<RowDataPacket[]>(
+        `SELECT o.id, o.userId, JSON_ARRAYAGG(p.id) AS products
+        FROM Trybesmith.Orders o
+        INNER JOIN Trybesmith.Products p ON o.id = p.orderId
+        GROUP BY o.userId, o.id;`,
+      );
+  
+      const orders = rows as IOrderResult[];
+  
+      return orders;
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   };
 
-  public create: (userId: number) => Promise<IOrder> = async (userId): Promise<IOrder> => {
-    const result = await this.connection.execute<ResultSetHeader>(
-      'INSERT INTO Trybesmith.Orders (userId) VALUES (?);',
-      [userId],
-    );
-    const [dataInserted] = result;
-    const { insertId } = dataInserted;
-    return {
-      id: insertId,
-      userId,
-    };
+  public create: (userId: number) => Promise<IOrder | undefined>
+  = async (userId): Promise<IOrder | undefined> => {
+    try {
+      const result = await this.connection.execute<ResultSetHeader>(
+        'INSERT INTO Trybesmith.Orders (userId) VALUES (?);',
+        [userId],
+      );
+      const [dataInserted] = result;
+      const { insertId } = dataInserted;
+      return {
+        id: insertId,
+        userId,
+      };
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   };
 }
