177dd2e93e4834a6664d6d0d4a121525ffbbcffe
Flávio Bianchetti <fjb.developer@gmail.com>
2022-04-01 15:35:38 -0300

    refactor: products model - type handling changed

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -8,34 +8,52 @@ export default class ProductsModel {
     this.connection = connection;
   }
 
-  public getAll: () => Promise<IProduct[]> = async (): Promise<IProduct[]> => {
-    const [result] = await this.connection
-      .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.Products;');
-    return result as IProduct[];
+  public getAll: () => Promise<IProduct[] | undefined>
+  = async (): Promise<IProduct[] | undefined> => {
+    try {
+      const [result] = await this.connection
+        .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.Products;');
+      return result as IProduct[];
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   };
 
-  public create: (p: IProduct) => Promise<IProduct> = async (product): Promise<IProduct> => {
-    const { name, amount } = product;
-    const result = await this.connection.execute<ResultSetHeader>(
-      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?);',
-      [name, amount],
-    );
-    const [dataInserted] = result;
-    const { insertId } = dataInserted;
-    return { id: insertId, ...product };
+  public create: (p: IProduct) => Promise<IProduct | undefined>
+  = async (product): Promise<IProduct | undefined> => {
+    try {
+      const { name, amount } = product;
+      const result = await this.connection.execute<ResultSetHeader>(
+        'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?);',
+        [name, amount],
+      );
+      const [dataInserted] = result;
+      const { insertId } = dataInserted;
+      return { id: insertId, ...product };
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   };
 
-  public getById: (id: number) => Promise<IProduct> = async (id): Promise<IProduct> => {
-    const [result] = await this.connection
-      .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.Products WHERE id = ?;', [id]);
-    const [rows] = result;
-    return rows as IProduct;
+  public getById: (id: number) => Promise<IProduct | undefined>
+  = async (id): Promise<IProduct | undefined> => {
+    try {
+      const [result] = await this.connection
+        .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.Products WHERE id = ?;', [id]);
+      const [rows] = result;
+      return rows as IProduct;
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   };
 
   public update: (p: IProduct) => Promise<IProduct | undefined> 
   = async (product: IProduct): Promise<IProduct | undefined> => {
-    const { id, name, amount, orderId } = product;
     try {
+      const { id, name, amount, orderId } = product;
       await this.connection.execute<ResultSetHeader>(
         'UPDATE Trybesmith.Products SET name = ?, amount = ?, orderId = ? WHERE id = ?;',
         [name, amount, orderId, id],
